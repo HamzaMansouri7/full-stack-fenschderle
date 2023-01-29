30e8282ceba2ed9d0f541333234f527cc1c1ea1a
HamzaMansouri7 <mansouris2020@gmail.com>
2023-01-29 05:53:12 +0100

    feat: main

---
+++ api/src/pizza/dto/pizza.dto.ts
@@ -0,0 +1,21 @@
+// import { ApiProperty } from '@nestjs/swagger';
+// import { IsNotEmpty, IsNumber, IsBoolean } from 'class-validator';
+
+// export class PizzaDto {
+
+//     @ApiProperty()
+//     @IsNotEmpty()
+//     readonly name: string;
+
+//     @ApiProperty()
+//     @IsNumber()
+//     readonly price: number;
+
+//     @ApiProperty()
+//     @IsNotEmpty()
+//     readonly pizzaTypeId: string;
+
+//     @ApiProperty()
+//     @IsNotEmpty()
+//     readonly pizzaSizeId: string;
+// }
---
+++ api/src/pizza/dto/pizzaType.dto.ts
@@ -0,0 +1,11 @@
+// import { ApiProperty } from "@nestjs/swagger";
+
+// export class PizzaTypeDto {
+
+//     @ApiProperty()
+//     readonly name: string;
+
+//     @ApiProperty()
+//     readonly description?: string;
+
+// }
