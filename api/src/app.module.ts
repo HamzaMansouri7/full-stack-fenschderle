78a1864848b1599ce6c8195b0ac241155e60bbff
RGhassen <rghassen@outlook.fr>
2023-02-14 02:33:09 +0100

    feat : order entites

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PastaModule } from './pasta/pasta.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [ PastaModule],
+  imports: [ PastaModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
